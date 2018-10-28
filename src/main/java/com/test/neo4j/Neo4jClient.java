70e3d1869d0f79d7d1add5d9200212bcd2f68dff
me <837544713@qq.com>
2018-10-28 11:20:41 +0800

    init

--- src/main/java/com/test/neo4j/Neo4jClient.java
+++ src/main/java/com/test/neo4j/Neo4jClient.java
@@ -27,7 +27,7 @@ public class Neo4jClient {
             //System.out.println(record.);
 
         }
-        System.out.println("master1");
+        System.out.println("sunhao1");
         session.close();
         driver.close();
     }
